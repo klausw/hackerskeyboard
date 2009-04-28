b6713f27c58d3f7b3c5593ad22a41936d7e477f7
Brandon Ballinger <Brandon Ballinger@localhost>
2009-04-28 11:38:42 -0700

    AI 147900: Set symbols state when setting keyboard in LatinIME [bug fix for 1815418].   BUG=1815418
    Automated import of CL 147900

---
+++ src/com/android/inputmethod/latin/LatinIMESettings.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2008-2009 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.android.inputmethod.latin;
+
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceGroup;
+import android.text.AutoText;
+
+public class LatinIMESettings extends PreferenceActivity {
+    
+    private static final String QUICK_FIXES_KEY = "quick_fixes";
+    private static final String SHOW_SUGGESTIONS_KEY = "show_suggestions";
+    private static final String PREDICTION_SETTINGS_KEY = "prediction_settings";
+    
+    private CheckBoxPreference mQuickFixes;
+    private CheckBoxPreference mShowSuggestions;
+    
+    @Override
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.prefs);
+        mQuickFixes = (CheckBoxPreference) findPreference(QUICK_FIXES_KEY);
+        mShowSuggestions = (CheckBoxPreference) findPreference(SHOW_SUGGESTIONS_KEY);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        int autoTextSize = AutoText.getSize(getListView());
+        if (autoTextSize < 1) {
+            ((PreferenceGroup) findPreference(PREDICTION_SETTINGS_KEY))
+                .removePreference(mQuickFixes);
+        } else {
+            mShowSuggestions.setDependency(QUICK_FIXES_KEY);
+        }
+    }
+}
