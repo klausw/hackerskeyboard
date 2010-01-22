635327c73d798a4261f756f041ebe5df579b6a00
Amith Yamasani <yamasani@google.com>
2010-01-21 17:00:46 -0800

    Compress the dictionaries so they are loaded into memory fully.
    This is an experiment to see if flash contention is causing occasional
    keyboard hicups that people complain about.
    
    A side effect is that the APK (with 5 languages) reduces in size by 25%.

--- Android.mk
+++ Android.mk
@@ -11,7 +11,7 @@ LOCAL_CERTIFICATE := shared
 
 LOCAL_JNI_SHARED_LIBRARIES := libjni_latinime
 
-LOCAL_AAPT_FLAGS := -0 .dict
+#LOCAL_AAPT_FLAGS := -0 .dict
 
 include $(BUILD_PACKAGE)
 include $(LOCAL_PATH)/dictionary/Android.mk
