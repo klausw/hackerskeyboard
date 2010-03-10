f33af5c267cbad55974b82d430fe4fd9dc8fee09
Amith Yamasani <yamasani@google.com>
2010-03-10 11:22:35 -0800

    Add cleanstep to avoid having 2 LatinIMEs in the build
    Change-Id: I862ca642f0f68bbead6e65e04e2e418c6acb45e5

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -44,6 +44,9 @@
 #$(call add-clean-step, find $(OUT_DIR) -type f -name "IGTalkSession*" -print0 | xargs -0 rm -f)
 #$(call add-clean-step, rm -rf $(PRODUCT_OUT)/data/*)
 
+$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/LatinIME*)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/LatinIME.apk)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
