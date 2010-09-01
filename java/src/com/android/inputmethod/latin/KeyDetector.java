f855ea2f0a14edee23798de1a1aabe71b1b90993
Tadashi G. Takaoka <takaoka@google.com>
2010-09-01 23:18:39 +0900

    Make abstract KeyDetector class
    The KeyDetector abstracts key detection algorithm. The
    ProximityKeyDetector is one of its concrete implementations.  Another
    one that might be called XAxisKeyDetector will follow to realize the
    pop-up mini-keyboard behavior described in bug#2959169.
    
    Bug: 2959169
    Change-Id: Idd3fc53282e6b721ec7a4ce500af8aba21ce07a3

---
+++ java/src/com/android/inputmethod/latin/KeyDetector.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.android.inputmethod.latin;
+
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.Keyboard.Key;
+
+import java.util.List;
+
+abstract class KeyDetector {
+    protected Keyboard mKeyboard;
+    protected Key[] mKeys;
+
+    protected boolean mProximityCorrectOn;
+    protected int mProximityThresholdSquare;
+
+    public Key[] setKeyboard(Keyboard keyboard) {
+        if (keyboard == null)
+            throw new NullPointerException();
+        mKeyboard = keyboard;
+        List<Key> keys = mKeyboard.getKeys();
+        Key[] array = keys.toArray(new Key[keys.size()]);
+        mKeys = array;
+        return array;
+    }
+
+    public void setProximityCorrectionEnabled(boolean enabled) {
+        mProximityCorrectOn = enabled;
+    }
+
+    public boolean isProximityCorrectionEnabled() {
+        return mProximityCorrectOn;
+    }
+
+    public void setProximityThreshold(int threshold) {
+        mProximityThresholdSquare = threshold * threshold;
+    }
+
+    abstract public int[] newCodeArray();
+
+    abstract public int getKeyIndexAndNearbyCodes(int x, int y, int[] allKeys);
+}
