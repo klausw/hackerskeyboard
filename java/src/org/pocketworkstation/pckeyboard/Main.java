c0099f103d4c41f4bf61dc7a43ca7874ed3c0243
Klaus Weidner <klausw@google.com>
2011-05-25 15:49:04 -0700

    Continuing on default branch.

---
+++ java/src/org/pocketworkstation/pckeyboard/Main.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.pocketworkstation.pckeyboard;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TextView.BufferType;
+
+public class Main extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        String html = getString(R.string.main_body);
+        Spanned content = Html.fromHtml(html);
+        TextView description = (TextView) findViewById(R.id.main_description);
+        description.setMovementMethod(LinkMovementMethod.getInstance());
+        description.setText(content, BufferType.SPANNABLE);
+
+
+        final Button setup1 = (Button) findViewById(R.id.main_setup_btn_1);
+        setup1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                startActivityForResult(new Intent(android.provider.Settings.ACTION_INPUT_METHOD_SETTINGS), 0);
+            }
+        });
+
+        final Button setup2 = (Button) findViewById(R.id.main_setup_btn_2);
+        setup2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                mgr.showInputMethodPicker();
+            }
+        });
+    }
+}
