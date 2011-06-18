393a65b4bb3d94a4efbcda139fd7220fb8500285
Darren Salt <linux@youmustbejoking.demon.co.uk>
2011-06-18 01:03:11 +0100

    Compose key (long press on dpad centre button).
    Compose strings borrowed from X.org.

---
+++ java/GetLanguages.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+getLangsForFiles () {
+	echo "en"  # default language
+	for F in "$@"
+	do
+		find res/ -name "$F"
+	done \
+	| sed -n 's/.*res.[a-z]*-\(..\(-...\)*\).*/\1/p'
+}
+
+getLangsForDicts () {
+	ls ../Dicts \
+	| sed 's/.*-//; s/.dict//'
+}
+
+makeStrings () {
+	Name="$1"
+	shift
+	echo "    private static final String[] $Name = {"
+	echo $(
+		for F in "$@"; do echo "$F"; done \
+		| sort -u
+	) \
+	| sed 's/ /", "/g; s/^/"/; s/$/"/' \
+	| fmt -w 70 \
+	| sed 's/^/        /'
+	echo "    };"
+	echo
+}
+
+LOCS=$(getLangsForFiles donottranslate-altchars.xml strings.xml)
+DICTS=$(getLangsForDicts)
+
+makeStrings KBD_LOCALIZATIONS $LOCS $DICTS
+
+makeStrings KBD_5_ROW $(getLangsForFiles donottranslate-keymap.xml)
+
+makeStrings KBD_4_ROW $(getLangsForFiles kbd_qwerty.xml)
