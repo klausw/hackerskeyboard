5c1731c6cc2eff57d032b647cca3e82ee4036f66
Klaus Weidner <klausw@google.com>
2011-09-25 16:07:32 -0700

    add Thai 5-row layout (no 4-row available)
    Contributed by user "hmmbug", thank you!

--- java/src/org/pocketworkstation/pckeyboard/InputLanguageSelection.java
+++ java/src/org/pocketworkstation/pckeyboard/InputLanguageSelection.java
@@ -56,7 +56,7 @@ public class InputLanguageSelection extends PreferenceActivity {
 
     private static final String[] KBD_5_ROW = {
         "ar", "cs", "de", "el", "en", "en-rDV", "en-rGB", "es", "fi", "fr",
-        "fr-rCA", "hr", "iw", "ro", "ru", "si", "sl", "sv", "uk"
+        "fr-rCA", "hr", "iw", "ro", "ru", "si", "sl", "sv", "th", "uk"
     };
 
     private static final String[] KBD_4_ROW = {
