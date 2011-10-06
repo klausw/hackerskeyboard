e4e27887cbeaa452711a85e87cab2fbb3a102ad4
Klaus Weidner <klausw@google.com>
2011-10-06 13:59:32 -0700

    Check-keymap scripts: add export to text format
    This is part of the planned migration to non-XML editable layout definitions.

--- java/CheckMaps.sh
+++ java/CheckMaps.sh
@@ -4,13 +4,21 @@ Res=res/
 Alt=donottranslate-altchars.xml
 Map=donottranslate-keymap.xml
 
-for Dir in res/values-*
+Out=assets/kbd/
+mkdir -p "$Out"
+
+for Dir in res/values res/values-*
 do
-  [ -f $Dir/$Map -o -f $Dir/$Alt ] || continue
+  [ -f $Dir/$Map ] || continue # -o -f $Dir/$Alt ] || continue
   Args="$Res/values/$Alt"
   [ -f $Dir/$Alt ] && Args="$Args $Dir/$Alt"
   Args="$Args $Res/values/$Map"
   [ -f $Dir/$Map ] && Args="$Args $Dir/$Map"
-  echo -n "$Dir: "
-  perl CheckMap.pl $Args
+  if [ -n "$CONVERT_MAPS" ]; then
+    Loc=$(echo "$Dir" | sed 's/res.values-*//; s/\/$//; s/^$/en/')
+    perl CheckMap.pl -c $Args > "$Out/map-full-$Loc.txt"
+  else
+    echo >&2 -n "$Dir: "
+    perl CheckMap.pl $Args
+  fi
 done
