0c59fec23cc47bf31413c1a1286d77a6c97f8f9f
Klaus Weidner <klausw@google.com>
2011-10-08 15:24:48 -0700

    Add automatic version info to Main activity and Settings

---
+++ java/AutoVersion.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+Ver=$(Hg-ident)
+exec > res/values/auto-version.xml
+
+cat <<'EOF'
+<?xml version="1.0" encoding="utf-8"?>
+<!-- Auto-generated file, do not edit -->
+<resources xmlns:xliff="urn:oasis:names:tc:xliff:document:1.2">
+EOF
+echo '   <string name="auto_version">'"$Ver"'</string>'
+echo '</resources>'
