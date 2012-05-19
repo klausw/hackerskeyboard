7f714732dbc11eca43579d1a8930e129e12542b9
Klaus Weidner <klausw@google.com>
2012-05-18 22:49:04 -0700

    Remove old x86 SDK hack

--- java/jni/Android.mk
+++ java/jni/Android.mk
@@ -8,10 +8,10 @@ LOCAL_SRC_FILES := \
 	src/dictionary.cpp \
 	src/char_utils.cpp
 
-ifneq ($(TARGET_ARCH),x86)
-LOCAL_NDK_VERSION := 4
-LOCAL_SDK_VERSION := 8
-endif
+#ifneq ($(TARGET_ARCH),x86)
+#LOCAL_NDK_VERSION := 4
+#LOCAL_SDK_VERSION := 8
+#endif
 
 LOCAL_MODULE := libjni_pckeyboard
 
