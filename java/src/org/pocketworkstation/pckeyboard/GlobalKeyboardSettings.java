6414d4b3b75ad745cef93368feb94b70f2a4c16c
Klaus Weidner <klausw@google.com>
2012-07-03 23:41:56 -0700

    Fix language-specific suggested punctuation.
    If the user didn't override the suggested punctuation list manually, use the
    localized list.
    
    See issue 225.

--- java/src/org/pocketworkstation/pckeyboard/GlobalKeyboardSettings.java
+++ java/src/org/pocketworkstation/pckeyboard/GlobalKeyboardSettings.java
@@ -153,7 +153,7 @@ public final class GlobalKeyboardSettings {
 
         addStringPref("pref_suggested_punctuation", new StringPref() {
             public void set(String val) { suggestedPunctuation = val; }
-            public String getDefault() { return res.getString(R.string.suggested_punctuations); }
+            public String getDefault() { return res.getString(R.string.suggested_punctuations_default); }
             public int getFlags() { return FLAG_PREF_NEW_PUNC_LIST; }
         });
 
