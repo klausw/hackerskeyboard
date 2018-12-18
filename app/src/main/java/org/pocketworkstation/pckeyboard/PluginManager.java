b5d5a2227a9a1ae86352221016d127e97c1a96ad
Parthiv Vora <psvora94@gmail.com>
2018-12-18 04:57:37 -0500

    fix anysoft lang pack support

--- app/src/main/java/org/pocketworkstation/pckeyboard/PluginManager.java
+++ app/src/main/java/org/pocketworkstation/pckeyboard/PluginManager.java
@@ -1,5 +1,19 @@
 package org.pocketworkstation.pckeyboard;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.content.res.XmlResourceParser;
+import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParserException;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -7,48 +21,154 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.xmlpull.v1.XmlPullParserException;
-
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.res.Resources;
-import android.content.res.XmlResourceParser;
-import android.util.Log;
-
 public class PluginManager extends BroadcastReceiver {
     private static String TAG = "PCKeyboard";
     private static String HK_INTENT_DICT = "org.pocketworkstation.DICT";
     private static String SOFTKEYBOARD_INTENT_DICT = "com.menny.android.anysoftkeyboard.DICTIONARY";
     private LatinIME mIME;
-    
+
     // Apparently anysoftkeyboard doesn't use ISO 639-1 language codes for its locales?
     // Add exceptions as needed.
     private static Map<String, String> SOFTKEYBOARD_LANG_MAP = new HashMap<String, String>();
+
     static {
         SOFTKEYBOARD_LANG_MAP.put("dk", "da");
     }
-    
-    PluginManager(LatinIME ime) {
-    	super();
-    	mIME = ime;
-    }
-    
+
     private static Map<String, DictPluginSpec> mPluginDicts =
-        new HashMap<String, DictPluginSpec>();
-    
+            new HashMap<String, DictPluginSpec>();
+
+    PluginManager(LatinIME ime) {
+        super();
+        mIME = ime;
+    }
+
     static interface DictPluginSpec {
         BinaryDictionary getDict(Context context);
+    }
+
+    static void getSoftKeyboardDictionaries(PackageManager packageManager) {
+        Intent dictIntent = new Intent(SOFTKEYBOARD_INTENT_DICT);
+        List<ResolveInfo> dictPacks = packageManager.queryBroadcastReceivers(
+                dictIntent, PackageManager.GET_RECEIVERS);
+        for (ResolveInfo ri : dictPacks) {
+            ApplicationInfo appInfo = ri.activityInfo.applicationInfo;
+            String pkgName = appInfo.packageName;
+            boolean success = false;
+            try {
+                Resources res = packageManager.getResourcesForApplication(appInfo);
+                //Log.i(TAG, "Found dictionary plugin package: " + pkgName);
+                int dictId = res.getIdentifier("dictionaries", "xml", pkgName);
+                if (dictId == 0) continue;
+                XmlResourceParser xrp = res.getXml(dictId);
+
+                String assetName = null;
+                int resId = 0;
+                String lang = null;
+                try {
+                    int current = xrp.getEventType();
+                    while (current != XmlResourceParser.END_DOCUMENT) {
+                        if (current == XmlResourceParser.START_TAG) {
+                            String tag = xrp.getName();
+                            if (tag != null) {
+                                if (tag.equals("Dictionary")) {
+                                    lang = xrp.getAttributeValue(null, "locale");
+                                    String convLang = SOFTKEYBOARD_LANG_MAP.get(lang);
+                                    if (convLang != null) lang = convLang;
+                                    String type = xrp.getAttributeValue(null, "type");
+                                    if (type == null || type.equals("raw") || type.equals("binary") || type.equals("binary_resource")) {
+                                        assetName = xrp.getAttributeValue(null, "dictionaryAssertName"); // sic
+                                        resId = xrp.getAttributeResourceValue(null, "dictionaryResourceId", 0);
+                                    } else {
+                                        Log.w(TAG, "Unsupported AnySoftKeyboard dict type " + type);
+                                    }
+                                    //Log.i(TAG, "asset=" + assetName + " lang=" + lang);
+                                }
+                            }
+                        }
+                        xrp.next();
+                        current = xrp.getEventType();
+                    }
+                } catch (XmlPullParserException e) {
+                    Log.e(TAG, "Dictionary XML parsing failure");
+                } catch (IOException e) {
+                    Log.e(TAG, "Dictionary XML IOException");
+                }
+
+                if ((assetName == null && resId == 0) || lang == null) continue;
+                DictPluginSpec spec = new DictPluginSpecSoftKeyboard(pkgName, assetName, resId);
+                mPluginDicts.put(lang, spec);
+                Log.i(TAG, "Found plugin dictionary: lang=" + lang + ", pkg=" + pkgName);
+                success = true;
+            } catch (NameNotFoundException e) {
+                Log.i(TAG, "bad");
+            } finally {
+                if (!success) {
+                    Log.i(TAG, "failed to load plugin dictionary spec from " + pkgName);
+                }
+            }
+        }
+    }
+
+    static void getHKDictionaries(PackageManager packageManager) {
+        Intent dictIntent = new Intent(HK_INTENT_DICT);
+        List<ResolveInfo> dictPacks = packageManager.queryIntentActivities(dictIntent, 0);
+        for (ResolveInfo ri : dictPacks) {
+            ApplicationInfo appInfo = ri.activityInfo.applicationInfo;
+            String pkgName = appInfo.packageName;
+            boolean success = false;
+            try {
+                Resources res = packageManager.getResourcesForApplication(appInfo);
+                //Log.i(TAG, "Found dictionary plugin package: " + pkgName);
+                int langId = res.getIdentifier("dict_language", "string", pkgName);
+                if (langId == 0) continue;
+                String lang = res.getString(langId);
+                int[] rawIds = null;
+
+                // Try single-file version first
+                int rawId = res.getIdentifier("main", "raw", pkgName);
+                if (rawId != 0) {
+                    rawIds = new int[]{rawId};
+                } else {
+                    // try multi-part version
+                    int parts = 0;
+                    List<Integer> ids = new ArrayList<Integer>();
+                    while (true) {
+                        int id = res.getIdentifier("main" + parts, "raw", pkgName);
+                        if (id == 0) break;
+                        ids.add(id);
+                        ++parts;
+                    }
+                    if (parts == 0) continue; // no parts found
+                    rawIds = new int[parts];
+                    for (int i = 0; i < parts; ++i) rawIds[i] = ids.get(i);
+                }
+                DictPluginSpec spec = new DictPluginSpecHK(pkgName, rawIds);
+                mPluginDicts.put(lang, spec);
+                Log.i(TAG, "Found plugin dictionary: lang=" + lang + ", pkg=" + pkgName);
+                success = true;
+            } catch (NameNotFoundException e) {
+                Log.i(TAG, "bad");
+            } finally {
+                if (!success) {
+                    Log.i(TAG, "failed to load plugin dictionary spec from " + pkgName);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Package information changed, updating dictionaries.");
+        getPluginDictionaries(context);
+        Log.i(TAG, "Finished updating dictionaries.");
+        mIME.toggleLanguage(true, true);
     }
 
     static private abstract class DictPluginSpecBase
             implements DictPluginSpec {
         String mPackageName;
-        
+
         Resources getResources(Context context) {
             PackageManager packageManager = context.getPackageManager();
             Resources res = null;
@@ -79,7 +199,7 @@ public class PluginManager extends BroadcastReceiver {
 
     static private class DictPluginSpecHK
             extends DictPluginSpecBase {
-        
+
         int[] mRawIds;
 
         public DictPluginSpecHK(String pkg, int[] ids) {
@@ -97,141 +217,45 @@ public class PluginManager extends BroadcastReceiver {
             return streams;
         }
     }
-    
+
     static private class DictPluginSpecSoftKeyboard
             extends DictPluginSpecBase {
-        
-        String mAssetName;
 
-        public DictPluginSpecSoftKeyboard(String pkg, String asset) {
+        String mAssetName;
+        int mResId;
+
+        public DictPluginSpecSoftKeyboard(String pkg, String asset, int resId) {
             mPackageName = pkg;
             mAssetName = asset;
+            mResId = resId;
         }
 
         @Override
         InputStream[] getStreams(Resources res) {
-            if (mAssetName == null) return null;
-            try {
-                InputStream in = res.getAssets().open(mAssetName);
-                return new InputStream[] {in};
-            } catch (IOException e) {
-                Log.e(TAG, "Dictionary asset loading failure");
-                return null;
-            }
-        }
-    }
-    
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        Log.i(TAG, "Package information changed, updating dictionaries.");
-        getPluginDictionaries(context);
-        Log.i(TAG, "Finished updating dictionaries.");
-        mIME.toggleLanguage(true, true);
-    }
-
-    static void getSoftKeyboardDictionaries(PackageManager packageManager) {
-        Intent dictIntent = new Intent(SOFTKEYBOARD_INTENT_DICT);
-        List<ResolveInfo> dictPacks = packageManager.queryBroadcastReceivers(
-        		dictIntent, PackageManager.GET_RECEIVERS);
-        for (ResolveInfo ri : dictPacks) {
-            ApplicationInfo appInfo = ri.activityInfo.applicationInfo;
-            String pkgName = appInfo.packageName;
-            boolean success = false;
-            try {
-                Resources res = packageManager.getResourcesForApplication(appInfo);
-                //Log.i(TAG, "Found dictionary plugin package: " + pkgName);
-                int dictId = res.getIdentifier("dictionaries", "xml", pkgName);
-                if (dictId == 0) continue;
-                XmlResourceParser xrp = res.getXml(dictId);
-
-                String assetName = null;
-                String lang = null;
+            if (mAssetName == null) {
+                if (mResId == 0) return null;
+                TypedArray a = res.obtainTypedArray(mResId);
+                int[] resIds;
                 try {
-                    int current = xrp.getEventType();
-                    while (current != XmlResourceParser.END_DOCUMENT) {
-                        if (current == XmlResourceParser.START_TAG) {
-                            String tag = xrp.getName();
-                            if (tag != null) {
-                                if (tag.equals("Dictionary")) {
-                                    lang = xrp.getAttributeValue(null, "locale");
-                                    String convLang = SOFTKEYBOARD_LANG_MAP.get(lang);
-                                    if (convLang != null) lang = convLang;
-                                    String type = xrp.getAttributeValue(null, "type");
-                                    if (type == null || type.equals("raw") || type.equals("binary")) {
-                                        assetName = xrp.getAttributeValue(null, "dictionaryAssertName"); // sic
-                                    } else {
-                                        Log.w(TAG, "Unsupported AnySoftKeyboard dict type " + type);
-                                    }
-                                    //Log.i(TAG, "asset=" + assetName + " lang=" + lang);
-                                }
-                            }
-                        }
-                        xrp.next();
-                        current = xrp.getEventType();
+                    resIds = new int[a.length()];
+                    for (int i = 0; i < a.length(); ++i) {
+                        resIds[i] = a.getResourceId(i, 0);
                     }
-                } catch (XmlPullParserException e) {
-                    Log.e(TAG, "Dictionary XML parsing failure");
+                } finally {
+                    a.recycle();
+                }
+                InputStream[] in = new InputStream[resIds.length];
+                for (int i = 0; i < resIds.length; ++i) {
+                    in[i] = res.openRawResource(resIds[i]);
+                }
+                return in;
+            } else {
+                try {
+                    InputStream in = res.getAssets().open(mAssetName);
+                    return new InputStream[]{in};
                 } catch (IOException e) {
-                    Log.e(TAG, "Dictionary XML IOException");
-                }
-
-                if (assetName == null || lang == null) continue;
-                DictPluginSpec spec = new DictPluginSpecSoftKeyboard(pkgName, assetName);
-                mPluginDicts.put(lang, spec);
-                Log.i(TAG, "Found plugin dictionary: lang=" + lang + ", pkg=" + pkgName);
-                success = true;
-            } catch (NameNotFoundException e) {
-                Log.i(TAG, "bad");
-            } finally {
-                if (!success) {
-                    Log.i(TAG, "failed to load plugin dictionary spec from " + pkgName);
-                }
-            }
-        }
-    }
-
-    static void getHKDictionaries(PackageManager packageManager) {
-        Intent dictIntent = new Intent(HK_INTENT_DICT);
-        List<ResolveInfo> dictPacks = packageManager.queryIntentActivities(dictIntent, 0);
-        for (ResolveInfo ri : dictPacks) {
-            ApplicationInfo appInfo = ri.activityInfo.applicationInfo;
-            String pkgName = appInfo.packageName;
-            boolean success = false;
-            try {
-                Resources res = packageManager.getResourcesForApplication(appInfo);
-                //Log.i(TAG, "Found dictionary plugin package: " + pkgName);
-                int langId = res.getIdentifier("dict_language", "string", pkgName);
-                if (langId == 0) continue;
-                String lang = res.getString(langId);
-                int[] rawIds = null;
-                
-                // Try single-file version first
-                int rawId = res.getIdentifier("main", "raw", pkgName);
-                if (rawId != 0) {
-                    rawIds = new int[] { rawId };
-                } else {
-                    // try multi-part version
-                    int parts = 0;
-                    List<Integer> ids = new ArrayList<Integer>();
-                    while (true) {
-                        int id = res.getIdentifier("main" + parts, "raw", pkgName);
-                        if (id == 0) break;
-                        ids.add(id);
-                        ++parts;
-                    }
-                    if (parts == 0) continue; // no parts found
-                    rawIds = new int[parts];
-                    for (int i = 0; i < parts; ++i) rawIds[i] = ids.get(i);
-                }
-                DictPluginSpec spec = new DictPluginSpecHK(pkgName, rawIds);
-                mPluginDicts.put(lang, spec);
-                Log.i(TAG, "Found plugin dictionary: lang=" + lang + ", pkg=" + pkgName);
-                success = true;
-            } catch (NameNotFoundException e) {
-                Log.i(TAG, "bad");
-            } finally {
-                if (!success) {
-                    Log.i(TAG, "failed to load plugin dictionary spec from " + pkgName);
+                    Log.e(TAG, "Dictionary asset loading failure");
+                    return null;
                 }
             }
         }
@@ -243,7 +267,7 @@ public class PluginManager extends BroadcastReceiver {
         getSoftKeyboardDictionaries(packageManager);
         getHKDictionaries(packageManager);
     }
-    
+
     static BinaryDictionary getDictionary(Context context, String lang) {
         //Log.i(TAG, "Looking for plugin dictionary for lang=" + lang);
         DictPluginSpec spec = mPluginDicts.get(lang);
